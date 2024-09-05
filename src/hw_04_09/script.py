4887b3af7dfeb90662ccfa6411efa751753a4fed
Michael Tzur <mltzur66@gamil.com>
2024-09-05 21:25:08 +0300

    darft

--- src/hw_04_09/script.py
+++ src/hw_04_09/script.py
@@ -1,2 +1,6 @@
-def greet(name):
-    print(f"Hello, {name}!")    
+def first_question(user_id, first_name, last_name, height, year_of_birth):
+    print(f"#id: {user_id} name: {last_name:<8} , {first_name:<8} height: {height:<6} year-of-birth: {year_of_birth:<4}")
+
+first_question("1", "Yosi", "Aaron", 160.56, 1990)
+first_question("1", "Shimon", "Tzur", 170.76, 1998)
+first_question("1", "Hen", "Shmueli", 190.96, 1999)
--- src/main.py
+++ src/main.py
@@ -1,3 +1,2 @@
-from hw_04_09.script import greet
+# from hw_04_09.script import first_question
 
-greet("dddd")
