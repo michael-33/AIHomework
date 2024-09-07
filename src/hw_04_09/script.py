ce373181788b24e5d0fb35ff85613a063a511ef2
Michael Tzur <mltzur66@gamil.com>
2024-09-07 22:32:52 +0300

    draft

--- src/hw_04_09/script.py
+++ src/hw_04_09/script.py
@@ -5,6 +5,7 @@ def first_question(user_id: str, first_name: str, last_name: str, height: float,
 		f"#id: {user_id} name: {last_name:<8} , {first_name:<8} height: {height:<6} year-of-birth: {year_of_birth:<4}")
 
 
+# user input
 first_question("1", "Yosi", "Aaron", 160.56, 1990)
 first_question("1", "Shimon", "Tzur", 170.76, 1998)
 first_question("1", "Hen", "Shmueli", 190.96, 1999)
@@ -41,6 +42,7 @@ def second_question(grade: int):
 			print("grade illegal")
 
 
+# user input
 second_question(70)
 second_question(700)
 
@@ -71,6 +73,7 @@ def third_question(volume: int):
 			print("volume illegal")
 
 
+# user input
 third_question(1)
 third_question(2)
 third_question(9)
